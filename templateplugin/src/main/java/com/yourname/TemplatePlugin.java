042e7a2e11d9e4cd838e054c77bb110ef41c6f4f
Kasejohnson01 <KaseJohnson01@gmail.com>
2025-01-14 18:43:55 -0600

    Updated version and names

--- templateplugin/src/main/java/com/yourname/TemplatePlugin.java
+++ templateplugin/src/main/java/com/yourname/TemplatePlugin.java
@@ -1,7 +1,15 @@
 package com.yourname;
 
-public class TemplatePlugin {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class TemplatePlugin extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        getLogger().info("TemplatePlugin has been enabled!");
+    }
+
+    public void onDisable() {
+        getLogger().info("TemplatePlugin has been disabled!");
     }
 }
